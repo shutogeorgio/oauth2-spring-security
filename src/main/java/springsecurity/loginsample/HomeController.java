c98d76f5c5d33038442ad6bec02932911a186a65
shuGeorgio <dev.shudiscrete@gmail.com>
2020-09-24 10:09:05 +0900

    mods: controller

--- src/main/java/springsecurity/loginsample/HomeController.java
+++ src/main/java/springsecurity/loginsample/HomeController.java
@@ -2,6 +2,7 @@ package springsecurity.loginsample;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,7 @@ import static org.springframework.http.ResponseEntity.ok;
 @RestController
 public class HomeController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public ResponseEntity<String> getHome(){
         String res = "Hello, You're at Home";
         return ok().contentType(MediaType.TEXT_PLAIN).body(res);
